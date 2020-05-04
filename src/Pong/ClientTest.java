44aa1d13dd8de429caaa0e9718667b0c693af714
IsaiahBlanks <noaaisaiah@gmail.com>
2020-05-04 14:59:45 -0400

    Merge branch 'master' of C:\Users\noaai\Desktop\Programming\PongGit\Pong with conflicts.

---
+++ src/Pong/ClientTest.java
@@ -0,0 +1,8 @@
+package Pong;
+
+public class ClientTest {
+    public static void main(String[] args) {
+        PongClient pong = new PongClient("localhost");
+        pong.runClient();
+    }
+}
---
+++ src/Pong/ServerTest.java
@@ -0,0 +1,8 @@
+package Pong;
+
+public class ServerTest {
+    public static void main(String[] args) {
+        PongServer server = new PongServer();
+        server.runServer();
+    }
+}
